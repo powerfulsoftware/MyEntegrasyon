66dc2e3a0ebe4f0c536de06373bf3a145f29d017
CELENK <CELENK@DESKTOP-MK1QIB0>
2024-03-04 23:54:22 +0300

    2024.03.04_1

--- MyEntegrasyon/Controllers/HomeController.cs
+++ MyEntegrasyon/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using MyEntegrasyon.Models;
+using System;
 using System.Diagnostics;
+using System.Net;
 
 namespace MyEntegrasyon.Controllers
 {
@@ -15,6 +17,29 @@ namespace MyEntegrasyon.Controllers
 
         public IActionResult Index()
         {
+            string? url = "";
+            url = "http://95.70.226.23:1515/(S(fjcangjis432kyhkvtblqxia))/IntegratorService/Connect";
+            var webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.Method = "GET";
+            webRequest.ContentType = "application/json";
+            webRequest.UserAgent = "Mozilla/5.0 (Windows NT 5.1; rv:28.0) Gecko/20100101 Firefox/28.0";
+            webRequest.ContentLength = 0;
+
+            var webResponse = (HttpWebResponse)webRequest.GetResponse();
+
+            if (webResponse.StatusCode != HttpStatusCode.OK) // olumsuzsa
+            {
+
+                var gelen = webResponse.Headers.ToString();
+            }
+            using (StreamReader reader = new StreamReader(webResponse.GetResponseStream()))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+                reader.Close();
+                webRequest.Abort();
+            }
+
+
             return View();
         }
 
