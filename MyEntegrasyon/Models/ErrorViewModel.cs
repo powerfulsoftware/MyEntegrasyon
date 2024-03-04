232ae9cfab9119787c2f306b2205b65803b135c5
CELENK <CELENK@DESKTOP-MK1QIB0>
2024-03-04 23:00:12 +0300

    Add project files.

---
+++ MyEntegrasyon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyEntegrasyon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
